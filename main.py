cea6b49865ee74a150125c7fec14a0cecc477509
Aditya <aditiwari451@gmail.com>
2024-09-12 11:23:45 +0530

    Initial commit

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def load_user(user_id):
     return db.get_or_404(User, user_id)
 
 
-# For adding profile images to the comment section
+
 gravatar = Gravatar(app,
                     size=100,
                     rating='g',
